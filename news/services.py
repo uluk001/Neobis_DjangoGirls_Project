47521181c8428563c00eb9f1e1fd6e7cc04fa1ff
Hacker <Hacker@MacBook-Pro-2.local>
2023-07-28 13:13:28 +0600

    Moved the controller functionality to services.py

--- news/services.py
+++ news/services.py
@@ -1,5 +1,6 @@
 from .models import Article
 
 def get_articles():
+    """Получаем все объекты с модели Article"""
     queryset = Article.objects.all()
     return queryset
--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import main
 
 urlpatterns = [
-    path('', main, name='home'),
+    path('', main, name='home'), 
 ]
